1d4fcb6fa15576c767879e640bc59b3afa29be97
romandolores <romanndolores@gmail.com>
2023-01-16 11:31:45 -0700

    no emeny yet

---
+++ FORSTBITE - game.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+onready var finish_area := $Map/FinishLine
+onready var player := $character
+
+func _ready() -> void:
+	finish_area.connect("body_entered", self, "_on_Finish_body_entered")
+
+func finish_game() -> void:
+	set_process(false)
+	player.set_physics_process(false)
+
+func _on_Finish_body_entered(body: Node) -> void:
+	finish_game()
