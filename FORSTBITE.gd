b249bd8f0e6abd2fff2e007fab9274d3b39884a8
romandolores <romanndolores@gmail.com>
2023-01-23 20:20:07 -0700

    progress with finish line

--- FORSTBITE.gd
+++ FORSTBITE.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-onready var finish_area := $Map/FinishLine
+onready var finish_area := $FinishLine
 onready var player := $character
 
 func _ready() -> void:
@@ -10,5 +10,5 @@ func finish_game() -> void:
 	set_process(false)
 	player.set_physics_process(false)
 
-func _on_Finish_body_entered(body: Node) -> void:
-	finish_game()
+#func _on_Finish_body_entered(body: Node) -> void:
+	#finish_game()
